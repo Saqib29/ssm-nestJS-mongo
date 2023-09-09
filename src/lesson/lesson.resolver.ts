d3ae334fefb6d29595d5ac6a592d4ab13aff1d41
saqib <saqib29111@gmail.com>
2023-09-09 23:52:29 +0600

    validation CreateLessonInput

--- src/lesson/lesson.resolver.ts
+++ src/lesson/lesson.resolver.ts
@@ -1,6 +1,7 @@
 import { Resolver, Query, Mutation, Args } from '@nestjs/graphql';
 import { LessonType } from './lesson.types';
 import { LessonService } from './lesson.service';
+import { CreateLessonInput } from './lesson.input';
 
 @Resolver(of => LessonType)
 export class LessonResolver {
@@ -14,12 +15,8 @@ export class LessonResolver {
     }
 
     @Mutation(returns => LessonType)
-    createLesson(
-        @Args('name') name: string,
-        @Args('startDate') startDate: string,
-        @Args('endDate') endDate: string
-    ){
-        return this.lessonService.createLesson(name, startDate, endDate);
+    createLesson(@Args('createLessonInput') createLessonInput: CreateLessonInput){
+        return this.lessonService.createLesson(createLessonInput);
     }
 
 }
